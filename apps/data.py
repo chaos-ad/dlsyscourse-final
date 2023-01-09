e76dae10a9d4a0ff2861983e0b46a533e2aa21a4
Anatoly Kanivetskiy <chaos-ad@yandex.ru>
2023-01-09 22:36:05 +0000

    overfit torchrec model on a single batch...
    ... just to ensure we have trainable arch & set of params

--- apps/data.py
+++ apps/data.py
@@ -2,8 +2,12 @@ import os
 import logging
 import numpy as np
 import pandas as pd
+
 import pyarrow
 import pyarrow.dataset
+
+import torch
+import torchrec.sparse.jagged_tensor
 
 from needle.autograd import Tensor
 
@@ -24,11 +28,16 @@ class Criteo1TBDatasetBatchIter:
         day_from = 0,
         day_to = 23,
         batch_size=1024,
-        columns = apps.etl.TARGET_COLUMNS + apps.etl.DENSE_COLUMNS,
+        target_column = apps.etl.TARGET_COLUMN,
+        dense_columns = apps.etl.DENSE_COLUMNS,
+        sparse_columns = apps.etl.SPARSE_IDX_COLUMNS,
+        sparse_buckets = None,
+        sparse_limits = None,
         filter = None,
         limit_records = 0,
         limit_batches = 0,
-        as_numpy = False
+        output_format = "needle",
+        device = None
     ):
         logger.debug("initializing new iterator...")
         s3_bucket = s3_bucket or os.environ['AWS_S3_BUCKET']
@@ -38,10 +47,24 @@ class Criteo1TBDatasetBatchIter:
         self.filter = (pyarrow.dataset.field('day') >= day_from) & (pyarrow.dataset.field('day') <= day_to)
         self.records = self.dataset.count_rows(filter=self.filter)
         self.batch_size = batch_size
-        self.columns = apps.etl.TARGET_COLUMNS + apps.etl.DENSE_COLUMNS + apps.etl.SPARSE_IDX_COLUMNS
+        self.columns = [target_column] + dense_columns + sparse_columns
         self.limit_batches = limit_batches
         self.limit_records = limit_records
-        self.as_numpy = as_numpy
+        self.output_format = output_format
+        self.device = device
+        self.sparse_limits = self._init_sparse_attribute(sparse_limits, sparse_columns)
+        self.sparse_buckets = self._init_sparse_attribute(sparse_buckets, sparse_columns)
+    
+    @staticmethod
+    def _init_sparse_attribute(attr, sparse_columns):
+        if attr is not None:
+            if isinstance(attr, int):
+                return [attr] * len(sparse_columns)
+            else:
+                assert isinstance(attr, list) and len(attr) == len(sparse_columns)
+                return np.array(attr).reshape((1, len(sparse_columns)))
+        else:
+            return None
 
     def __iter__(self):
         logger.debug("starting iteration...")
@@ -49,6 +72,53 @@ class Criteo1TBDatasetBatchIter:
         self.read_batches = 0
         self.read_records = 0
         return self
+    
+    def to_torch(self, batch):
+        (X_dense, X_sparse, Y_true) = batch
+
+        batch_size = Y_true.shape[0]
+
+        X_dense = torch.tensor(X_dense, dtype=torch.float32, device=self.device)
+
+        CAT_FEATURE_COUNT = len(apps.etl.SPARSE_IDX_COLUMNS)
+        num_ids_in_batch = CAT_FEATURE_COUNT * batch_size
+        length_per_key = CAT_FEATURE_COUNT * [batch_size]
+        offset_per_key = [batch_size * i for i in range(CAT_FEATURE_COUNT + 1)]
+        index_per_key = {key: i for (i, key) in enumerate(apps.etl.SPARSE_IDX_COLUMNS)}
+        lengths = torch.ones((num_ids_in_batch * 2,), dtype=torch.int32)
+        offsets = torch.arange(0, num_ids_in_batch * 2 + 1, dtype=torch.int32)
+
+        X_sparse = torchrec.sparse.jagged_tensor.KeyedJaggedTensor(
+            keys = apps.etl.SPARSE_IDX_COLUMNS,
+            values = torch.from_numpy(X_sparse.transpose(1, 0).reshape(-1)).to(self.device),
+            lengths = lengths[:num_ids_in_batch],
+            offsets = offsets[: num_ids_in_batch + 1],
+            stride = batch_size,
+            length_per_key=length_per_key,
+            offset_per_key=offset_per_key,
+            index_per_key=index_per_key
+        )
+        Y_true = torch.tensor(Y_true.reshape(-1), dtype=torch.float32, device=self.device)
+
+        batch = (X_dense, X_sparse, Y_true)
+        return batch
+
+    def to_needle(self, batch):
+        (X_dense, X_sparse, Y_true) = batch
+        Y_true = Tensor(Y_true, device=device, requires_grad=False)
+        X_dense = Tensor(X_dense, device=device, requires_grad=False)
+        X_sparse = Tensor(X_sparse, device=device, requires_grad=False)
+        batch = (X_dense, X_sparse, Y_true)
+        return batch
+
+    def to_output(self, batch):
+        if self.output_format == 'needle':
+            return self.to_needle(batch)
+        elif self.output_format == 'torch':
+            return self.to_torch(batch)
+        elif self.output_format == 'numpy':
+            return batch
+        raise Exception(f"unsupported output format {self.output_format}")
 
     def __next__(self):
         logger.debug("fetching next batch...")
@@ -59,19 +129,46 @@ class Criteo1TBDatasetBatchIter:
         batch = next(self.batch_iter).to_pandas()
         self.read_batches += 1
         self.read_records += batch.shape[0]
-        Y = batch[apps.etl.TARGET_COLUMN].to_numpy()
         X_dense = batch[apps.etl.DENSE_COLUMNS].to_numpy()
         X_sparse = batch[apps.etl.SPARSE_IDX_COLUMNS].to_numpy()
-        if not self.as_numpy:
-            Y = Tensor(Y, requires_grad=False)
-            X_dense = Tensor(X_dense, requires_grad=False)
-            X_sparse = Tensor(X_sparse, requires_grad=False)
-        return (X_dense, X_sparse, Y)
+        Y_true = batch[apps.etl.TARGET_COLUMN].to_numpy()
+
+        X_dense = np.log(X_dense + 3)
+        if self.sparse_limits is not None:
+            X_sparse[X_sparse > self.sparse_limits] = 0
+        if self.sparse_buckets is not None:
+            X_sparse %= self.sparse_buckets
+
+        batch = (X_dense, X_sparse, Y_true)
+        return self.to_output(batch)
 
     def __len__(self) -> int:
         return self.records
 
 def read_s3_dataset(**kwargs):
     return Criteo1TBDatasetBatchIter(**kwargs)
+
+##############################################################################
+
+class RepeatFirstBatch():
+    def __init__(self, dataset, repeat):
+        self.batch = next(iter(dataset))
+        self.limit_batches = repeat
+    
+    def __iter__(self):
+        self.read_batches = 0
+        return self
+    
+    def __next__(self):
+        if self.read_batches >= self.limit_batches:
+            raise StopIteration()
+        self.read_batches += 1
+        return self.batch
+    
+    def __len__(self) -> int:
+        return 1
+
+def repeat_first_batch(dataset, repeat):
+    return RepeatFirstBatch(dataset, repeat)
 
 ##############################################################################
--- tests/debug.py
+++ tests/debug.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath("./python"))
 import torch.nn
 import torchrec
 import torchrec.models.dlrm
-import torchrec.sparse.jagged_tensor
 import torcheval.metrics
 
 import tqdm
@@ -30,179 +29,216 @@ logger = logging.getLogger("tests.debug")
 
 ##############################################################################
 
-def train_torch():
-    dense_arch_layer_sizes = [16]  # last dim must match the embedding size
-    over_arch_layer_sizes = [32,1]
-    learning_rate = 0.1 ## also try 4.0, 15.0
-    weight_decay = 0.0
+def torch_train(model, loss_fn, optimizer, dataset, epoch_id=None, with_pbar=None):
 
+    num_samples = 0
+    num_batches = 0
+    avg_loss = 0
+    auroc = torcheval.metrics.BinaryAUROC()
+    accuracy = torcheval.metrics.BinaryAccuracy()
+
+    model.train()
+    pbar = tqdm.tqdm(desc=f"TRAIN[{epoch_id=}]", total=len(dataset)) if with_pbar else None
+    for batch_id, (X_dense, X_sparse, Y_true) in enumerate(dataset, start=1):
+        batch_size = Y_true.shape[0]
+        logger.debug(f"TRAIN[{epoch_id=}] on {batch_id=} of size {batch_size}...")
+
+        optimizer.zero_grad()
+
+        Y_logits = model(X_dense, X_sparse).squeeze(-1)
+        loss = loss_fn(Y_logits, Y_true)
+
+        loss.backward()
+        optimizer.step()
+
+        Y_pred = torch.sigmoid(Y_logits)
+        auroc.update(Y_pred, Y_true)
+        accuracy.update(Y_pred, Y_true)
+        
+        cur_loss = float(loss.detach().numpy())
+        avg_loss = (avg_loss * num_batches + cur_loss) / (num_batches + 1)
+
+        num_batches += 1
+        num_samples += batch_size
+
+        if pbar:
+            pbar.update(batch_size)
+
+        logger.debug(f"TRAIN[{epoch_id=}] on {batch_id=} of size {batch_size}: done ({cur_loss=:0.4f}, {avg_loss=:0.4f}")
+    
+    auroc_val = auroc.compute().item()
+    accuracy_val = accuracy.compute().item()
+    logger.info(f"TRAIN[{epoch_id=}] done: {avg_loss=:0.4f}, {auroc_val=:0.4f}, {accuracy_val=:0.4f}, {num_samples=}, {num_batches=}")
+    return (avg_loss, auroc_val, accuracy_val, num_samples, num_batches)
+
+def torch_eval(model, loss_fn, dataset, epoch_id=None, with_pbar=True):
+
+    num_samples = 0
+    num_batches = 0
+    avg_loss = 0
+    auroc = torcheval.metrics.BinaryAUROC()
+    accuracy = torcheval.metrics.BinaryAccuracy()
+
+    model.eval()
+    with torch.no_grad():
+        pbar = tqdm.tqdm(desc=f"EVAL[{epoch_id=}]", total=len(dataset)) if with_pbar else None
+        for batch_id, (X_dense, X_sparse, Y_true) in enumerate(dataset, start=1):
+            batch_size = Y_true.shape[0]
+            logger.debug(f"EVAL[{epoch_id=}] on {batch_id=} of size {batch_size}...")
+
+            Y_logits = model(X_dense, X_sparse).squeeze(-1)
+            loss = loss_fn(Y_logits, Y_true)
+
+            Y_pred = torch.sigmoid(Y_logits)
+            auroc.update(Y_pred, Y_true)
+            accuracy.update(Y_pred, Y_true)
+
+            cur_loss = float(loss.detach().numpy())
+            avg_loss = (avg_loss * num_batches + cur_loss) / (num_batches + 1)
+
+            num_batches += 1
+            num_samples += batch_size
+
+            if pbar:
+                pbar.update(batch_size)
+
+            logger.debug(f"EVAL[{epoch_id=}] on {batch_id=} of size {batch_size}: done ({cur_loss=:0.4f}, {avg_loss=:0.4f}")
+
+        auroc_val = auroc.compute().item()
+        accuracy_val = accuracy.compute().item()
+        logger.info(f"EVAL[{epoch_id=}] done: {avg_loss=:0.4f}, {auroc_val=:0.4f}, {accuracy_val=:0.4f}, {num_samples=}, {num_batches=}")
+        return (avg_loss, auroc_val, accuracy_val, num_samples, num_batches)
+
+def torch_dataset(day=None, **kwargs):
+    kwargs['day_from'] = day if day is not None else kwargs.get('day_from', 0)
+    kwargs['day_to'] = day if day is not None else kwargs.get('day_to', 23)
+    kwargs['batch_size'] = kwargs.get('batch_size', 1024)
+    kwargs['device'] = kwargs.get('device', torch.device("cpu"))
+    
+    result = apps.data.read_s3_dataset(
+        s3_prefix="anatoly/datasets/criteo-terabyte-click-log", 
+        s3_path="preprocessed/joined",
+        output_format = "torch",
+        **kwargs
+    )
+    result = apps.data.repeat_first_batch(result, 1)
+    return result
+
+def run_torch(
+    embedding_dim = 16,
+    # embedding_dim = 128,
+    # num_embeddings_per_feature = [45833188,36746,17245,7413,20243,3,7114,1441,62,29275261,1572176,345138,10,2209,11267,128,4,974,14,48937457,11316796,40094537,452104,12606,104,35],
+    # num_embeddings_per_feature = [2000000,36746,17245,7413,20243,3,7114,1441,62,2000000,1572176,345138,10,2209,11267,128,4,974,14,2000000,2000000,2000000,452104,12606,104,35],
+    num_embeddings_per_feature = 1000,
+    over_arch_layer_sizes = [32,32,16,1],
+    dense_arch_layer_sizes = [64,32,16],
+    # over_arch_layer_sizes = [1024,1024,512,256,1],
+    # dense_arch_layer_sizes = [512,256,128],
+    epochs = 1000,
+    batch_size = 16384,
+    learning_rate = 0.5, # 15.0,
+    # change_lr = True,
+    # lr_change_point = 0.65,
+    # lr_after_change_point = 0.035,
+    with_pbar = False
+):
     device = torch.device("cpu")
+
+    train_dataset = torch_dataset(day = 0, batch_size = batch_size, limit_batches = 100, device = device, sparse_buckets = num_embeddings_per_feature)
+    eval_dataset = torch_dataset(day = 22, batch_size = batch_size, limit_batches = 100, device = device, sparse_buckets = num_embeddings_per_feature)
+    test_dataset = torch_dataset(day = 23, batch_size = batch_size, limit_batches = 100, device = device, sparse_buckets = num_embeddings_per_feature)
+    logger.debug(f"size of a train dataset: {len(train_dataset)}")
+    logger.debug(f"size of a eval dataset: {len(eval_dataset)}")
+    logger.debug(f"size of a test dataset: {len(test_dataset)}")
 
     model = torchrec.models.dlrm.DLRM(
         embedding_bag_collection=torchrec.EmbeddingBagCollection(
             tables=[
                 torchrec.EmbeddingBagConfig(
                     name = f"t_{feature_name}",
-                    embedding_dim = 16,
-                    num_embeddings = 1000,
-                    feature_names=[feature_name],
+                    embedding_dim = embedding_dim,
+                    num_embeddings = num_embeddings_per_feature[feature_idx] if isinstance(num_embeddings_per_feature, list) else num_embeddings_per_feature,
+                    feature_names = [feature_name],
                 )
                 for feature_idx, feature_name in enumerate(apps.etl.SPARSE_IDX_COLUMNS)
             ],
             device=device
         ),
-        dense_in_features=len(apps.etl.DENSE_COLUMNS),
-        dense_arch_layer_sizes=dense_arch_layer_sizes,
-        over_arch_layer_sizes=over_arch_layer_sizes,
-        dense_device=device
+        dense_in_features = len(apps.etl.DENSE_COLUMNS),
+        dense_arch_layer_sizes = dense_arch_layer_sizes,
+        over_arch_layer_sizes = over_arch_layer_sizes,
+        dense_device = device
     )
-
-    opt = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-    auroc = torcheval.metrics.BinaryAUROC(device=device)
 
     loss_fn = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    total_loss = 0
-    total_errors = 0
-    total_batches = 0
-    total_examples = 0
-
-    dataset_iter = apps.data.read_s3_dataset(
-        s3_prefix="anatoly/datasets/criteo-terabyte-click-log", 
-        s3_path="preprocessed/joined",
-        day_from = 0,
-        day_to = 0,
-        batch_size = 2048,
-        # limit_batches = 100,
-        as_numpy=True
-    )
-    pbar = tqdm.tqdm(desc="reading data", total=len(dataset_iter))
-    for batch_id, (X_dense, X_sparse, Y) in enumerate(dataset_iter, start=1):
-
-        batch_size = Y.shape[0]
-        # logger.debug(f"TRAIN on {batch_id=} of size {batch_size}...")
-        X_dense = np.log(X_dense + 3)
-        X_sparse[X_sparse > 999] = 0
-        
-        X_dense = torch.tensor(X_dense, dtype=torch.float32, device=device)
-        # X_sparse = torch.tensor(X_sparse, dtype=torch.float32, device=device)
+    for epoch_id in range(epochs):
+        torch_train(model, loss_fn, optimizer, train_dataset, epoch_id=epoch_id, with_pbar=with_pbar)
+        # torch_eval(model, loss_fn, eval_dataset, epoch_id=epoch_id, with_pbar=with_pbar)
+    torch_eval(model, loss_fn, test_dataset, epoch_id=epoch_id, with_pbar=with_pbar)
 
 
-        CAT_FEATURE_COUNT = len(apps.etl.SPARSE_IDX_COLUMNS)
-        _num_ids_in_batch = CAT_FEATURE_COUNT * (batch_size * 2)
-        num_ids_in_batch = CAT_FEATURE_COUNT * batch_size
-        length_per_key = CAT_FEATURE_COUNT * [batch_size]
-        offset_per_key = [batch_size * i for i in range(CAT_FEATURE_COUNT + 1)]
-        index_per_key = {key: i for (i, key) in enumerate(apps.etl.SPARSE_IDX_COLUMNS)}
-        lengths = torch.ones((_num_ids_in_batch,), dtype=torch.int32)
-        offsets = torch.arange(0, _num_ids_in_batch + 1, dtype=torch.int32)
+# def train_needle():
 
-        X_sparse = torchrec.sparse.jagged_tensor.KeyedJaggedTensor(
-            keys = apps.etl.SPARSE_IDX_COLUMNS,
-            values = torch.from_numpy(X_sparse.transpose(1, 0).reshape(-1)),
-            lengths = lengths[:num_ids_in_batch],
-            offsets = offsets[: num_ids_in_batch + 1],
-            stride = batch_size,
-            length_per_key=length_per_key,
-            offset_per_key=offset_per_key,
-            index_per_key=index_per_key
-        )
+#     device = ndl.cpu()
+#     dense_layer_sizes = [512, 256, 64]
+#     # interaction_layer_sizes = [512,512,256,1]
+#     learning_rate = 0.1 ## also try 4.0, 15.0
+#     weight_decay = 0.0
+#     model = apps.models.DLRM(
+#         dense_in_features = len(apps.etl.DENSE_COLUMNS),
+#         dense_layer_sizes=[512,256,64],
+#         device=device
+#     )
+#     model.train()
 
-        Y = torch.tensor(Y.reshape(-1), dtype=torch.float32, device=device)
+#     loss_fn = nn.BinaryCrossEntropyLoss()
+#     opt = ndl.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-        opt.zero_grad()
-        out = model(X_dense, X_sparse)
-        loss = loss_fn(out.squeeze(-1), Y)
-        
-        loss.backward()
-        opt.step()
+#     total_loss = 0
+#     total_errors = 0
+#     total_batches = 0
+#     total_examples = 0
 
-        # preds = torch.sigmoid(logits)
-        # auroc.update(preds, labels)
-        # auroc_result = auroc.compute().item()
-        # num_samples = torch.tensor(sum(map(len, auroc.targets)), device=device)
-        # print(f"AUROC over {stage} set: {auroc_result}.")
-        # print(f"Number of {stage} samples: {num_samples}")
+#     dataset_iter = apps.data.read_s3_dataset(
+#         s3_prefix="anatoly/datasets/criteo-terabyte-click-log", 
+#         s3_path="preprocessed/joined",
+#         day_from = 0,
+#         day_to = 0,
+#         batch_size = 1024,
+#         limit_batches = 100,
+#         output_format = "needle"
+#     )
+#     for batch_id, (X_dense, X_sparse, Y) in enumerate(dataset_iter, start=1):
+#         batch_size = Y.shape[0]
+#         logger.debug(f"TRAIN on {batch_id=} of size {batch_size}...")
+#         X_dense = np.log(X_dense + 3)
+#         X_dense = Tensor(X_dense, device=device, requires_grad=False)
+#         X_sparse = Tensor(X_sparse, device=device, requires_grad=False)
+#         Y = Tensor(Y.flatten(), device=device, requires_grad=False)
 
-        y_prob = out.detach().numpy()
-        y_pred = (y_prob > 0)
-        errors = np.not_equal(y_pred, Y.numpy()).sum()
+#         opt.reset_grad()
+#         out = model(X_dense)
+#         out = out.reshape((out.shape[0],))
+#         loss = loss_fn(out, Y)
+#         loss.backward()
+#         opt.step()
 
-        cur_loss = float(loss.detach().numpy())
-        total_loss += cur_loss * batch_size
-        total_errors += errors
-        total_batches += 1
-        total_examples += batch_size
+#         y_prob = out.numpy()
+#         y_pred = (y_prob > 0)
+#         errors = np.not_equal(y_pred, Y.numpy()).sum()
 
-        avg_loss = (total_loss / total_examples)
-        avg_error_rate = (total_errors / total_examples)
+#         cur_loss = loss.numpy()[0]
+#         total_loss += cur_loss * batch_size
+#         total_errors += errors
+#         total_batches += 1
+#         total_examples += batch_size
 
-        pbar.update(batch_size)
+#         avg_loss = (total_loss / total_examples)
+#         avg_error_rate = (total_errors / total_examples)
 
-        # logger.debug(f"TRAIN on {batch_id=} of size {batch_size}: done ({cur_loss=:0.4f}, {avg_loss=:0.4f}, {avg_error_rate=:0.4f})")
-        
-
-def train_needle():
-
-    device = ndl.cpu()
-    dense_layer_sizes = [512, 256, 64]
-    # interaction_layer_sizes = [512,512,256,1]
-    learning_rate = 0.1 ## also try 4.0, 15.0
-    weight_decay = 0.0
-    model = apps.models.DLRM(
-        dense_in_features = len(apps.etl.DENSE_COLUMNS),
-        dense_layer_sizes=[512,256,64],
-        device=device
-    )
-    model.train()
-
-    loss_fn = nn.BinaryCrossEntropyLoss()
-    opt = ndl.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-
-    total_loss = 0
-    total_errors = 0
-    total_batches = 0
-    total_examples = 0
-
-    dataset_iter = apps.data.read_s3_dataset(
-        s3_prefix="anatoly/datasets/criteo-terabyte-click-log", 
-        s3_path="preprocessed/joined",
-        day_from = 0,
-        day_to = 0,
-        batch_size = 1024,
-        limit_batches = 100,
-        as_numpy=True
-    )
-    for batch_id, (X_dense, X_sparse, Y) in enumerate(dataset_iter):
-        batch_size = Y.shape[0]
-        logger.debug(f"TRAIN on {batch_id=} of size {batch_size}...")
-        X_dense = np.log(X_dense + 3)
-        X_dense = Tensor(X_dense, device=device, requires_grad=False)
-        X_sparse = Tensor(X_sparse, device=device, requires_grad=False)
-        Y = Tensor(Y.flatten(), device=device, requires_grad=False)
-
-        opt.reset_grad()
-        out = model(X_dense)
-        out = out.reshape((out.shape[0],))
-        loss = loss_fn(out, Y)
-        loss.backward()
-        opt.step()
-
-        y_prob = out.numpy()
-        y_pred = (y_prob > 0)
-        errors = np.not_equal(y_pred, Y.numpy()).sum()
-
-        cur_loss = loss.numpy()[0]
-        total_loss += cur_loss * batch_size
-        total_errors += errors
-        total_batches += 1
-        total_examples += batch_size
-
-        avg_loss = (total_loss / total_examples)
-        avg_error_rate = (total_errors / total_examples)
-
-        logger.debug(f"TRAIN on {batch_id=} of size {batch_size}: done ({cur_loss=}, {total_loss=}, {total_batches=}, {avg_loss=:0.4f}, {avg_error_rate=:0.4f})")
+#         logger.debug(f"TRAIN on {batch_id=} of size {batch_size}: done ({cur_loss=}, {total_loss=}, {total_batches=}, {avg_loss=:0.4f}, {avg_error_rate=:0.4f})")
         
 
 def main():
@@ -211,7 +247,7 @@ def main():
     assert dotenv.load_dotenv(dotenv_path="conf/dev.env")
     apps.utils.common.setup_logging(config_file="conf/logging.yml")
 
-    train_torch()
+    run_torch()
 
 if __name__ == '__main__':
     main()
